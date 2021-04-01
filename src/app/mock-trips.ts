e953dc4d3bf973ff7e07d337602fc86f96e38500
katemejaski <katmackjj@gmail.com>
2021-04-01 17:08:50 -0400

    First Commit

---
+++ src/app/mock-trips.ts
@@ -0,0 +1,11 @@
+import { Trip } from './trip';
+
+export const TRIPS: Trip[] = [
+  { id: 1, name: 'Shanta', location:'Egypt', description: 'Egypt (/ˈiːdʒɪpt/ (About this soundlisten) EE-jipt; Arabic: مِصر‎ Miṣr), officially the Arab Republic of Egypt, is a transcontinental country spanning the northeast corner of Africa and southwest corner of Asia by a land bridge formed by the Sinai Peninsula. Egypt is a Mediterranean country bordered by the Gaza Strip (Palestine) and Israel to the northeast, the Gulf of Aqaba and the Red Sea to the east, Sudan to the south, and Libya to the west. Across the Gulf of Aqaba lies Jordan, across the Red Sea lies Saudi Arabia, and across the Mediterranean lie Greece, Turkey and Cyprus, although none share a land border with Egypt',
+   image: './assets/shanta.jpg'},
+  { id: 2, name: 'Lydia', location:'The Maldives', description: '', image:'./assets/lydia.jpg'},
+  { id: 3, name: 'Kate', location:'Acadia National Park', description: '', image:'./assets/download.jpg'},
+  { id: 4, name: 'Damini', location: 'Orlando, Florida', description:'', image:'./assets/Damini.jpg'},
+  { id: 5, name: 'David', location:'The Moon', description: '', image:'./assets/dave.jpg'},
+  { id: 6, name: 'Diana', location: 'Tampa, Florida', description:'', image: ''}
+];
